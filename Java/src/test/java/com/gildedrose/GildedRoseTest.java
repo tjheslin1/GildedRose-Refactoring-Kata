7ef06f763302d74e27ddf98f4a2c63e6b464a355
Thomas Heslin <tjheslin1@gmail.com>
2017-07-31 17:59:50 +0100

    Added unit tests to cover and document existing functionality. Added travis build.

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,17 +1,154 @@
 package com.gildedrose;
 
-import static org.junit.Assert.*;
-
+import org.junit.Ignore;
 import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class GildedRoseTest {
 
     @Test
-    public void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+    public void qualityDegradesByOneWhilstDaysLeftOnSellByCounter() throws Exception {
+        Item[] items = new Item[]{new Item("item_oneDayLeft", 1, 5)};
         GildedRose app = new GildedRose(items);
+
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+
+        assertThat(items[0].name).isEqualTo("item_oneDayLeft");
+        assertThat(items[0].sellIn).isEqualTo(0);
+        assertThat(items[0].quality).isEqualTo(4);
     }
 
+    @Test
+    public void qualityDegradesTwiceAsFastAfterSellByDateHasPassed() throws Exception {
+        Item[] items = new Item[]{new Item("item_noDaysLeft", 0, 5)};
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertThat(items[0].name).isEqualTo("item_noDaysLeft");
+        assertThat(items[0].sellIn).isEqualTo(-1);
+        assertThat(items[0].quality).isEqualTo(3);
+    }
+
+    @Test
+    public void qualityOfAnItemIsNeverNegative() throws Exception {
+        Item[] items = new Item[]{new Item("item_noDaysLeft_noQuality", 0, 0)};
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertThat(items[0].name).isEqualTo("item_noDaysLeft_noQuality");
+        assertThat(items[0].sellIn).isEqualTo(-1);
+        assertThat(items[0].quality).isEqualTo(0);
+    }
+
+    @Test
+    public void agedBrieIncreasesInQualityCloserToTheSellInCounterReachingZero() throws Exception {
+        Item[] items = new Item[]{new Item("Aged Brie", 5, 10)};
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertThat(items[0].name).isEqualTo("Aged Brie");
+        assertThat(items[0].sellIn).isEqualTo(4);
+        assertThat(items[0].quality).isEqualTo(11);
+    }
+
+    @Test
+    public void theQualityOfQualityMaturingItemsNeverIncreaseHigherThanFifty() throws Exception {
+        Item[] items = new Item[]{
+                new Item("Aged Brie", 5, 50),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 10, 50)
+        };
+
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertThat(items[0].name).isEqualTo("Aged Brie");
+        assertThat(items[0].sellIn).isEqualTo(4);
+        assertThat(items[0].quality).isEqualTo(50);
+
+        assertThat(items[1].name).isEqualTo("Backstage passes to a TAFKAL80ETC concert");
+        assertThat(items[1].sellIn).isEqualTo(9);
+        assertThat(items[1].quality).isEqualTo(50);
+    }
+
+    @Test
+    public void legendaryItemSulfurasHasNoChangeToSellInOrQualityOverTime() throws Exception {
+        Item[] items = new Item[]{new Item("Sulfuras, Hand of Ragnaros", 5, 80)};
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertThat(items[0].name).isEqualTo("Sulfuras, Hand of Ragnaros");
+        assertThat(items[0].sellIn).isEqualTo(5);
+        assertThat(items[0].quality).isEqualTo(80);
+    }
+
+    // TODO table test to test all boundaries
+    @Test
+    public void backstagePassItemsQualityIncreasesByTwoWhenThereAreBetweenTenAndSixDaysLeft() throws Exception {
+        Item[] items = new Item[]{new Item("Backstage passes to a TAFKAL80ETC concert", 10, 40)};
+
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertThat(items[0].name).isEqualTo("Backstage passes to a TAFKAL80ETC concert");
+        assertThat(items[0].sellIn).isEqualTo(9);
+        assertThat(items[0].quality).isEqualTo(42);
+    }
+
+    // TODO table test to test all boundaries
+    @Test
+    public void backstagePassItemsQualityIncreasesByThreeWhenThereAreBetweenFiveAndZeroDaysLeft() throws Exception {
+        Item[] items = new Item[]{new Item("Backstage passes to a TAFKAL80ETC concert", 4, 40)};
+
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertThat(items[0].name).isEqualTo("Backstage passes to a TAFKAL80ETC concert");
+        assertThat(items[0].sellIn).isEqualTo(3);
+        assertThat(items[0].quality).isEqualTo(43);
+    }
+
+    @Test
+    public void backstagePassItemsQualityDropsToZeroWhenTheSellInDateIsPassed() throws Exception {
+        Item[] items = new Item[]{new Item("Backstage passes to a TAFKAL80ETC concert", 0, 40)};
+
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertThat(items[0].name).isEqualTo("Backstage passes to a TAFKAL80ETC concert");
+        assertThat(items[0].sellIn).isEqualTo(-1);
+        assertThat(items[0].quality).isEqualTo(0);
+    }
+
+    @Test
+    public void legendaryItemSulfurasAlwaysHasAQualityOfEighty() throws Exception {
+        Item[] items = new Item[]{new Item("Sulfuras, Hand of Ragnaros", 5, 80)};
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertThat(items[0].quality).isEqualTo(80);
+    }
+
+    @Ignore // TODO implement conjured items functionality
+    @Test
+    public void itemsWhosNameBeginWithConjuredDegradeTwiceAsFast() throws Exception {
+        Item[] items = new Item[]{new Item("Conjured Mana Cake", 3, 6)};
+
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertThat(items[0].name).isEqualTo("Conjured Mana Cake");
+        assertThat(items[0].sellIn).isEqualTo(2);
+        assertThat(items[0].quality).isEqualTo(4);
+    }
 }
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Gilded Rose Refactoring Kata
+# Gilded Rose Refactoring Kata  [![Build Status](https://travis-ci.org/tjheslin1/GildedRose-Refactoring-Kata.svg?branch=master)](https://travis-ci.org/tjheslin1/GildedRose-Refactoring-Kata)
 
 This Kata was originally created by Terry Hughes (http://twitter.com/#!/TerryHughes). It is already on GitHub [here](https://github.com/NotMyself/GildedRose). See also [Bobby Johnson's description of the kata](http://iamnotmyself.com/2011/02/13/refactor-this-the-gilded-rose-kata/).
 
@@ -18,7 +18,7 @@ You could write some unit tests yourself, using the requirements to identify sui
 
 Alternatively, use the "Text-Based" tests provided in this repository. (Read more about that in the next section)
 
-Whichever testing approach you choose, the idea of the exercise is to do some deliberate practice, and improve your skills at designing test cases and refactoring. The idea is not to re-write the code from scratch, but rather to practice designing tests, taking small steps, running the tests often, and incrementally improving the design. 
+Whichever testing approach you choose, the idea of the exercise is to do some deliberate practice, and improve your skills at designing test cases and refactoring. The idea is not to re-write the code from scratch, but rather to practice designing tests, taking small steps, running the tests often, and incrementally improving the design.
 
 ## Text-Based Approval Testing
 
@@ -46,4 +46,4 @@ I've also set this kata up on [cyber-dojo](http://cyber-dojo.org) for several la
 
 I analysed this repo according to the clean code standards on [Better Code Hub](https://bettercodehub.com) just to get an independent opinion of how bad the code is. Perhaps unsurprisingly, the compliance score is low!
 
-[![BCH compliance](https://bettercodehub.com/edge/badge/emilybache/GildedRose-Refactoring-Kata?branch=master)](https://bettercodehub.com/) 
+[![BCH compliance](https://bettercodehub.com/edge/badge/emilybache/GildedRose-Refactoring-Kata?branch=master)](https://bettercodehub.com/)
